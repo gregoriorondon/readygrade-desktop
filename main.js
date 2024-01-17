497251150dd7e3d303188cbd5d90a604c4576ca9
gregoriorondon <gregorioalexanderondon@gmail.com>
2024-01-17 13:34:09 -0400

    sync db form v3

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
-const { app, BrowserWindow } = require('electron');
-const url = require('url');
-const path = require('path');
-
-// if (process.env.NODE_ENV !== 'production') {
-//     require('electron-reload')(__dirname, {
-
-//     })
-// }
+const { app, BrowserWindow } = require('electron')
+const url = require('url')
+const path = require('path')
 
 let mainWindow
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({});
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'src/views/index.html'),
-        protocol: 'file',
-        slashes: true
-    }))
-    // mainWindow.on('closed', () => {
-    //     app2.quit();
-    // });
-});
-
+  mainWindow = new BrowserWindow({
+    with: 800,
+    height: 640
+  })
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'src/views/index.html'),
+    protocol: 'file',
+    slashes: true
+  }))
+})
